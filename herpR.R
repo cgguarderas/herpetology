06db9bdc92d7e8998ad41cb45d691a3702f5954f
CARLOS GUARDERAS <carlosguarderasr@gmail.com>
2017-10-17 11:49:02 -0500

    renyi index and plots
    mas indices y plots de algunas de las variables
    
    analizar

--- herpR.R
+++ herpR.R
@@ -11,6 +11,9 @@
 # realizar mapas de las areas muestreadas a traves de rasters
 # 
 # 
+# 
+#       boxplot de familias comparadas para las dos reservas
+#       
 
 install.packages("vegan")       # instalar programa vegan para analisis
 
@@ -47,7 +50,81 @@ diversity(herp1, index = "simpson")
 
                 ##      anzu            zunac   
                 ## [1] 0.9577778        0.8884688
-                ## 
 
+
+specnumber(herp1)
+
+H <- diversity(herp1)                           ##      Shannon index 
+H
+
+J <- H/log(specnumber(herp1))                   ##       Pielou’s evenness J = H0/log(S) 
+J
+
+        ##       
+        ##      R´enyi and Tsallis diversities
+        ##      
+
+k <- sample(nrow(herp1), 2)
+k
+
+R <- renyi(herp1[k,])
+R
+
+
+
+plot(R)
+
+
+##              The plot uses Trellis graphics with a 
+##              separate panel for each site. The dots 
+##              show the values for sites, and the lines
+##               the extremes and median in the data set.
+##               
+##               los graficos que obtenemos nos muestran tres lineas la mediana 
+##               y los extremos superior e inferior
+##               
+##               
+
+boxplot(R)
+
+##      α parameter of Fisher’s log-series can be used as a diversity index 
+##
+##
+
+alpha <- fisher.alpha(herp1)
+alpha
 
 save.image("~/1.USFQ/TESIS/HERPETO/HERPETO/herpRworkspace.RData")
+
+require(graphics)
+
+plot(density(c(-20, rep(0,98), 20)), xlim = c(-4, 4))  # IQR = 0
+
+# The Old Faithful geyser data
+d <- density(faithful$eruptions, bw = "sj")
+d
+plot(tabla_de_especies_herpeto)
+
+d <- density(herp1$)
+
+
+
+
+
+data(dune)
+data(dune.env)
+mod <- cca(dune ~ A1 + Moisture + Management, dune.env)
+plot(mod, type="n")
+text(mod, dis="cn")
+points(mod, pch=21, col="red", bg="yellow", cex=1.2)
+text(mod, "species", col="blue", cex=0.8)
+## Limited output of 'summary'
+head(summary(mod), tail=2)
+## Scaling can be numeric or more user-friendly names
+## e.g. Hill's scaling for (C)CA
+scrs <- scores(mod, scaling = "sites", hill = TRUE)
+## or correlation-based scores in PCA/RDA
+scrs <- scores(rda(dune ~ A1 + Moisture + Management, dune.env),
+               scaling = "sites", correlation = TRUE)
+
+
